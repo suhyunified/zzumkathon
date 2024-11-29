7eb8def0988d6294ffa1b3d8bbe20f6d2dafe316
suhyun-jobis <suhuun@jobis.co>
2024-11-29 11:59:19 +0900

    img

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,13 +5,20 @@ export { default as ImgDecoBell } from "./img_deco_bell.svg?react";
 export { default as ImgDecoSocksGreen } from "./img_deco_socks_green.svg?react";
 export { default as ImgDecoSocksRed } from "./img_deco_socks_red.svg?react";
 export { default as ImgDecoStick } from "./img_deco_stick.svg?react";
-import ImgDecoBallGreenSmall from "./img_deco_ball_green_small.png";
-import ImgDecoBallRedSmall from "./img_deco_ball_red_small.png";
-import ImgDecoBallYellowSmall from "./img_deco_ball_yellow_small.png";
-import ImgDecoBellSmall from "./img_deco_bell_small.png";
-import ImgDecoSocksGreenSmall from "./img_deco_socks_green_small.png";
-import ImgDecoSocksRedSmall from "./img_deco_socks_red_small.png";
-import ImgDecoStickSmall from "./img_deco_stick_small.png";
+export { default as ImgDecoBallGreenSmall } from "./img_deco_ball_green_small.svg?react";
+export { default as ImgDecoBallRedSmall } from "./img_deco_ball_red_small.svg?react";
+export { default as ImgDecoBallYellowSmall } from "./img_deco_ball_yellow_small.svg?react";
+export { default as ImgDecoBellSmall } from "./img_deco_bell_small.svg?react";
+export { default as ImgDecoSocksGreenSmall } from "./img_deco_socks_green_small.svg?react";
+export { default as ImgDecoSocksRedSmall } from "./img_deco_socks_red_small.svg?react";
+export { default as ImgDecoStickSmall } from "./img_deco_stick_small.svg?react";
+// import ImgDecoBallGreenSmall from "./img_deco_ball_green_small.png";
+// import ImgDecoBallRedSmall from "./img_deco_ball_red_small.png";
+// import ImgDecoBallYellowSmall from "./img_deco_ball_yellow_small.png";
+// import ImgDecoBellSmall from "./img_deco_bell_small.png";
+// import ImgDecoSocksGreenSmall from "./img_deco_socks_green_small.png";
+// import ImgDecoSocksRedSmall from "./img_deco_socks_red_small.png";
+// import ImgDecoStickSmall from "./img_deco_stick_small.png";
 export { default as IconChevronLeft } from "./icon_chevron_left.svg?react";
 export { default as IconChevronRight } from "./icon_chevron_right.svg?react";
 export { default as IconItem1 } from "./icon_item1.svg?react";
@@ -20,14 +27,4 @@ export { default as IconItem3 } from "./icon_item3.svg?react";
 import ImgStar from "./img_star.png";
 import ImgStarGray from "./img_star_gray.png";
 
-export {
-  ImgStar,
-  ImgStarGray,
-  ImgDecoBallGreenSmall,
-  ImgDecoBallRedSmall,
-  ImgDecoBallYellowSmall,
-  ImgDecoBellSmall,
-  ImgDecoSocksGreenSmall,
-  ImgDecoSocksRedSmall,
-  ImgDecoStickSmall,
-};
+export { ImgStar, ImgStarGray };
