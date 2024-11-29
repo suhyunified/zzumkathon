38d9672caf9baec86274c9ceca6eea8473c47d56
suhyun-jobis <suhuun@jobis.co>
2024-11-29 10:25:05 +0900

    api 주소 변경

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://d9e4-210-223-83-193.ngrok-free.app/api",
+  baseURL: "https://7aa1-210-223-83-193.ngrok-free.app/api",
   headers: { "Content-Type": "application/json" },
 });
